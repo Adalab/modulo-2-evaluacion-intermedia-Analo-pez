3d43388bf63d261e5906b9d379a840383b4907c1
Analo-pez <ana.ts.lopez@gmail.com>
2020-06-30 09:47:47 +0200

    add function for number

--- main.js
+++ main.js
@@ -1,2 +1,33 @@
 'use strict';
 
+const number = 100;
+let input = document.querySelector('.js-inputNumber');
+const button = document.querySelector('.js-btn');
+const textClue = document.querySelector('.js-clue');
+const attemps = document.querySelector('.js-attemps');
+console.log();
+
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
+console.log(getRandomNumber(number));
+
+// // function getANumber(event) {
+// //     if (input > number) {
+// //       return console.log(`Demasiado Alto`);
+
+// //     if (input< number) {
+// //       return console.log('Demasiado bajo');
+
+// //     if (input===number) {
+// //         return console.log('Has ganado campeona!');
+
+// //     if (input >100 || input<=0) {
+// //         return console.log('El número debe estar entre 1 y 100');
+// //         }
+// //     }
+// //   }
+
+// button.addEventListener('click', getANumber);
+
+//contador de intentos
